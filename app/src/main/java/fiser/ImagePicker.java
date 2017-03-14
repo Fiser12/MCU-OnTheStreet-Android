bf951313c4afdbf306964ac8239d2c7cae54f513
Fiser <fiser12@icloud.com>
2017-03-14 03:10:41 +0100

    Código de borrado hecho

--- app/src/main/java/fiser/ImagePicker.java
+++ app/src/main/java/fiser/ImagePicker.java
@@ -122,9 +122,6 @@ public class ImagePicker {
         return actuallyUsableBitmap;
     }
 
-    /**
-     * Resize to avoid using too much memory loading big images (e.g.: 2560*1920)
-     **/
     private static Bitmap getImageResized(Context context, Uri selectedImage) {
         Bitmap bm = null;
         int[] sampleSizes = new int[]{5, 3, 2, 1};
